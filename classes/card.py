d93d745d943bcce9cb17d53becd73efaf4c58157
tgnelson19 <nelson.tyler.g@gmail.com>
2023-08-28 19:47:21 -0500

    Very basic compaction of several methods

--- classes/card.py
+++ classes/card.py
@@ -13,7 +13,6 @@ class Card():
         self.posX = PX
         self.posY = PY
         
-        
         ##
         # Asthetic Attributes for the card
         ##
@@ -21,9 +20,14 @@ class Card():
         self.RGB = (R, G, B)
         self.fontSize = 15
         self.font = pygame.font.Font('media/freeFont.otf', self.fontSize)
+
+        self.cardSlotPositions = [(20,30), (140,30), (260,30), (380, 30), (20,660), (140,660), (260,660), (380, 660)]
         
         self.cardBackgroundColor = (50,50,50) #Grey background color
         
+        self.defaultCardWidth = 80
+        self.defaultCardHeight = 100
+
         self.cardWidth = 80
         self.cardHeight = 100
         
@@ -54,6 +58,7 @@ class Card():
         self.cardBackgroundBody = pygame.Rect(self.posX - self.xOff, self.posY- self.yOff, self.cardWidth, self.cardHeight)
         
 
+
     def drawCard(self, screen):
 
         pygame.draw.rect(screen, self.cardBackgroundColor, self.cardBackgroundBody)
@@ -61,11 +66,47 @@ class Card():
         
         screen.blit(self.healthText, self.healthRect)
         screen.blit(self.attackText, self.attackRect)
+
+
+
+    def cardMovementHandler(self, mouseDown):
         
-    def mouseClicked(self, mouseX, mouseY):
+        mouseX, mouseY = pygame.mouse.get_pos()
+
+        global currentHeldCard
         
-        self.posX = mouseX - (self.cardWidth / 2)
-        self.posY = mouseY - (self.cardHeight / 2)
+        if not mouseDown:
+            for pos in self.cardSlotPositions:
+                if self.cardBackgroundBody.collidepoint(pos[0] + (self.cardWidth / 2), pos[1] + (self.cardHeight / 2)):
+                    self.posX = pos[0] + 10
+                    self.posY = pos[1] 
+            
+            currentHeldCard = 0 #If mouse is not down, not holding any card
+
+        elif currentHeldCard != 0:
+            currentHeldCard.posX = mouseX - (self.cardWidth / 2)
+            currentHeldCard.posY = mouseY - (self.cardHeight / 2)
+        
+        if (self.cardBackgroundBody.collidepoint(mouseX, mouseY) and ((currentHeldCard == 0) or (currentHeldCard == self))):
+            
+            self.cardWidth = self.defaultCardWidth * 1.2
+            self.cardHeight = self.defaultCardHeight * 1.2 
+            
+            self.xOff = self.defaultCardWidth / 10
+            self.yOff = self.defaultCardHeight / 10
+
+            if (mouseDown): #If card is clicked 
+                currentHeldCard = self
+            
+        else:
+            
+            self.cardWidth = self.defaultCardWidth
+            self.cardHeight = self.defaultCardHeight
+            
+            self.xOff = 0
+            self.yOff = 0      
+            
+        
 
 
         
--- main.py
+++ main.py
@@ -4,84 +4,38 @@
 import pygame
 from classes.card import Card
 from classes.background import Background
+from classes.variables import Variables
 
 ###
 # Window Initializers
 ###
+
+vars = Variables()
+
 pygame.init() #Initializes a window
 
-sW, sH = 500, 1000 #Determines (s)creen (W)idth, and (s)creen (H)eigth
-screen = pygame.display.set_mode([sW, sH]) #Makes a screen that's that wide
-
-defaultCardWidth = 80
-defaultCardHeight = 100
-
-clock = pygame.time.Clock() #Main time keeper
-
-done = False #Determines if the game is over or not
-mouseDown = False
-currentHeldCard = 0
+screen = pygame.display.set_mode([vars.sW, vars.sH]) #Makes a screen that's that wide
 
 testingCard = Card() #Basic card helper
 greenCard = Card(12, 3, 100, 250, 0, 255, 0)
+
+currentHeldCard = 0
 
 cardList = [testingCard, greenCard]
 
 testingBackground = Background() #Basic background helper
 
-#playingCardList = [0]*10 #will this work?
-
-while not done: #While the game hasn't been closed (Main loop of the game, determines what is done each frame)
+while not vars.done: #While the game hasn't been closed (Main loop of the game, determines what is done each frame)
 
     testingBackground.drawBackground(screen)
 
-    for event in pygame.event.get(): #Main event handler
+    vars.eventHandler()
 
-        if event.type == pygame.QUIT: done = True #Close the entire program
-
-        if event.type == pygame.KEYDOWN: 
-            if event.key == pygame.K_ESCAPE: done = True
-            
-        if event.type == pygame.MOUSEBUTTONDOWN: mouseDown = True
-        if event.type == pygame.MOUSEBUTTONUP: mouseDown = False
-        
-    for card in cardList:
-        
-        mouseX, mouseY = pygame.mouse.get_pos()
-        
-        if not mouseDown:
-            currentHeldCard = 0 #If mouse is not down, not halding any card
-
-            for cardPos in testingBackground.cardPlacementSlotList:
-                if (card.cardBackgroundBody.collidepoint(mouseX, mouseY) and cardPos.collidepoint(mouseX, mouseY)):
-                    card.posX = cardPos.x
-                    card.posY = cardPos.y
-                    print(str(card.posX) + " " +  str(card.posY))  
-        
-        if (card.cardBackgroundBody.collidepoint(mouseX, mouseY) and ((currentHeldCard == 0) or (currentHeldCard == card))):
-            
-            card.cardWidth = defaultCardWidth * 1.2
-            card.cardHeight = defaultCardHeight * 1.2 
-            
-            card.xOff = defaultCardWidth / 10
-            card.yOff = defaultCardHeight / 10
-
-            if (mouseDown): #If card is clicked 
-                currentHeldCard = card
-                currentHeldCard.mouseClicked(mouseX, mouseY) #make sure to do current held card for rest so you move the card that you're holding on to    
-         
-        else:
-            
-            card.cardWidth = defaultCardWidth
-            card.cardHeight = defaultCardHeight
-            
-            card.xOff = 0
-            card.yOff = 0      
-            
     for card in cardList:
         card.updateCard() #foreach card would go here instead to get constant updates 
         card.drawCard(screen)
+        card.cardMovementHandler(vars.mouseDown)
 
     pygame.display.flip() #Displays currently drawn frame
     screen.fill(pygame.Color(0,0,0)) #Clears screen with a black color
-    clock.tick(60) #Keeps screen updated to 60 frames per second
+    vars.clock.tick(60) #Keeps screen updated to 60 frames per second
